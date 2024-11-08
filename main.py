cfaa6007921c1894cbc2b5a258df534a5c0a42a8
Mikołaj Zakrzewski <mikizakr@gmail.com>
2024-11-08 23:01:50 +0100

    Command line arguments template

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 import json
 import csv
 
+from argparse import ArgumentParser
+
 from sheep import Sheep
 from wolf import Wolf
 
 
 def main():
+    parser = ArgumentParser()
+    parser.add_argument("-c", "--config", nargs=1, metavar="FILE",
+                        help="An auxiliary configuration file, where %(metavar)s stands for a filename")
+    parser.add_argument("-l", "--log", nargs=1, metavar="LEVEL",
+                        help="Record events to a log, where %(metavar)s stands for a log level - DEBUG(10), INFO(20), "
+                             "WARNING(20), ERROR(20), or CRITICAL(50)")
+    parser.add_argument("-r", "--rounds", nargs=1, default=50, metavar="NUM",
+                        help="The maximum number of rounds, where %(metavar)s denotes an integer")
+    parser.add_argument("-s", "--sheep", nargs=1, default=15, metavar="NUM",
+                        help="The number of sheep, where %(metavar)s denotes an integer;")
+    parser.add_argument("-w", "--wait", action="store_true",
+                        help="Introduce a pause after displaying basic information about the status of the simulation "
+                             "at the end of each round until a key is pressed")
+    args = parser.parse_args()
+
     max_rounds = 50
     herd_size = 15
     wolf_move_distance = 1.0
@@ -48,6 +65,7 @@ def main():
 
         alive_sheep_number_by_round.append(alive_sheep_number)
 
+    # TODO: Save the data to the files after every round
     with open("pos.json", "w", encoding="utf-8") as file:
         json.dump(animal_positions_by_round, file, indent=4)
 
